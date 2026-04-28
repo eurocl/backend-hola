c6728c6c924f29e7c67de6230350070c0df5bee5
Somniloquios <europa.cid@gmail.com>
2026-04-28 19:38:53 -0300

    backend hola mundo

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/api/hola", (req, res) => {
+  res.json({ mensaje: "Hola mundo backend 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
